a4a97f3b3b93687bec6b2b9b9b2b68a9c1ae91a5
kevin <kevincamara95@gmail.com>
2025-11-01 05:09:58 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,27 @@
+// Cerrar el menú de navegación al hacer clic en un enlace (móvil)
+// Excepto cuando se hace clic en "Opciones" que debe desplegar el submenú
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', (e) => {
+        // No cerrar si es el enlace de Opciones
+        if (link.getAttribute('data-bs-toggle') === 'collapse') {
+            return;
+        }
+        
+        const navbarCollapse = document.getElementById('navbarNav');
+        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+        if (bsCollapse) {
+            bsCollapse.hide();
+        }
+    });
+});
+
+// Cerrar el menú al hacer clic en una opción del submenú
+document.querySelectorAll('.submenu-item').forEach(item => {
+    item.addEventListener('click', () => {
+        const navbarCollapse = document.getElementById('navbarNav');
+        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+        if (bsCollapse) {
+            bsCollapse.hide();
+        }
+    });
+});
